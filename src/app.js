1420210b52c72f2996cbee7e5f9d4bf1ac5064c6
MeednightPurple <eniluap.truocaled@gmail.com>
2023-12-31 20:20:51 +0100

    toggle add task panel on click

---
+++ src/app.js
@@ -0,0 +1,22 @@
+function showAddTask() {
+    const addTask = document.getElementById("add-task");
+    const addBtn = document.getElementById("add-btn")
+
+    if (addTask.style.display == "none") {
+        addTask.style.display = "flex";
+        addBtn.textContent = "Close";
+    } else {
+        addTask.style.display = "none";
+        addBtn.textContent = "Add";
+    }
+
+}
+
+window.onload = () => {
+    let a = 0;
+    document.getElementById("add-task").style.display = "none";
+
+    const addButton = document.getElementById("add-btn");
+    addButton.addEventListener("click", showAddTask);
+
+}
